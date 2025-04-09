075b393f49c7adaec1dbe3c08172c62b40c23e69
mohamad_qoreishi <mghorishi89@gmail.com>
2025-04-09 12:30:04 +0330

    بدون gui

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,6 @@
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
-public class Main extends Application {
+public class Main {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Falcon");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
     public static void main(String[] args) {
         new LoginMenu();
     }
