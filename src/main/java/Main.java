56e2d6229b43caf518e3d791c7259c0146033565
BitaEbrahimgol <bitaebrahimgol@gmail.com>
2025-04-14 20:05:06 +0330

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-// Class Runner
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        new LoginMenu();
+        // Ensure JavaFX is initialized
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            new LoginPage(primaryStage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
